803fd4ac03735409f7e203cc968419a8dfd14543
Funsy <egorabramovich2012@mail.ru>
2024-11-28 13:41:55 +0300

    Update _test.py

--- src/_test.py
+++ src/_test.py
@@ -1,17 +1,3 @@
-import time
-import sys
+import FlowInput
 
-time.sleep(0.5)
-
-for i in range(5):
-    
-    print(f'hello{i}', end='\r')
-    sys.stdout.write(f"\033[{i}G")
-    sys.stdout.flush()
-
-    time.sleep(0.5)
-
-    sys.stdout.write("\033[0G")
-    sys.stdout.flush()
-
-    time.sleep(0.5)
+FlowInput.in
